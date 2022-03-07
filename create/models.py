6fe3d82543cb4041eae8df0008e7263e68e8c044
28-08-2000 <dharamsinghmeena2000@gmail.com>
2022-03-07 19:21:56 +0530

    Created individual model, form, adding and saving new data

--- create/models.py
+++ create/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# create a class to store individuals info
+class individual(models.Model):
+	first_Name = models.CharField(max_length = 100)
+	last_Name = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return self.first_Name;
--- create/views.py
+++ create/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import individual
 
 # Create your views here.
 def create(request):
-	return render(request, 'create.html')
+
+	individuals = individual.objects.all()
+
+	# if request is post, get details and save them
+	if request.method == 'POST':
+
+		new_individual = individual(
+				first_Name = request.POST['first_name'],
+				last_Name = request.POST['last_name']
+			)
+		new_individual.save()
+
+		return redirect('/create')
+
+	context = {
+		'individuals': individuals
+	}
+
+	return render(request, 'create.html', context)
