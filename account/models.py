f0d1aa742b57400485258cb4298593ecd71ef60b
28-08-2000 <dharamsinghmeena2000@gmail.com>
2022-03-16 17:49:24 +0530

    created account app

---
+++ account/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+class MyAccountManager(BaseUserManager):
+	def create_user(self, email, username, password=None):
+		if not email:
+			raise ValueError('Users must have an email address')
+		if not username:
+			raise ValueError('Users must have a username')
+
+		user = self.model(
+			email=self.normalize_email(email),
+			username=username,
+		)
+
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
+
+class Account(AbstractBaseUser):
+	email 					= models.EmailField(verbose_name="email", max_length=60, unique=True)
+	username 				= models.CharField(max_length=30, unique=True)
+	date_joined				= models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+	last_login				= models.DateTimeField(verbose_name='last login', auto_now=True)
+	hide_email				= models.BooleanField(default=True)
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username']
+
+	objects = MyAccountManager()
+
+	def __str__(self):
+		return self.username
